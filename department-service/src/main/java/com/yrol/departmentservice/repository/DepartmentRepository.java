9ce349338050a08f70b3176c4a8604a8d89dbd24
Yrol <yrollgayanth@yahoo.com>
2023-05-23 22:26:47 +1000

    Adding and configuring DB, and creating the Department entity

---
+++ department-service/src/main/java/com/yrol/departmentservice/repository/DepartmentRepository.java
@@ -0,0 +1,7 @@
+package com.yrol.departmentservice.repository;
+
+import com.yrol.departmentservice.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
