f9370363e29652085cd2c47197b87fc5145d7209
Yrol <yrollgayanth@yahoo.com>
2023-06-29 23:23:16 +1000

    Adding the config server module

---
+++ config-server/src/test/java/com/yrol/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yrol.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
